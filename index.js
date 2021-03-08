25e9cbf016e8f62c0ec71d88462a88699a319374
Ivan <hammer.99.ivan@gmail.com>
2021-03-08 17:29:11 +0700

    simple backend

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
-mongoose.connect('mongodb+srv://ivan:217@cluster0.zafnn.mongodb.net/users', {
+mongoose.connect('mongodb+srv://ivan:217@cluster0.zafnn.mongodb.net/komadb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, () => {
@@ -13,6 +15,14 @@ const routes = require('./routes/routes')
 
 
 app = express()
+
+app.use(cookieParser())
+
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000']
+}))
+
 app.use(express.json())
 
 app.use('/api', routes)
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
 router.post('/register', async (req, res) => {
@@ -27,9 +28,60 @@ router.post('/login', async (req, res) => {
 
     if (!user) {
         return res.status(404).send({
-            message
+            message: 'User not found in DB'
         })
     }
+
+    if (await bcrypt.compare(req.body.password, user.password)) {
+        return res.status(400).send({
+            message: 'Invalid credentials'
+        })
+    }
+
+    const token = jwt.sign({ _id: user._id }, "secret")
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000 // 1 day cookie life
+    })
+
+    res.send({
+        message: 'success'
+    })
+
+})
+
+router.get('/user', async (req, res) => {
+
+    try {
+        const cookie = req.cookies['jwt']
+
+        const claims = jwt.verify(cookie, "secret")
+
+        if (!claims) {
+            return res.status(401).send({
+                message: 'Unauthorized'
+            })
+        }
+
+        const user = await User.findOne({ _id: claims._id })
+
+        const { password, ...data } = await user.toJSON()
+
+        res.send(data)
+    } catch (e) {
+        return res.status(401).send({
+            message: 'Unauthorized'
+        })
+    }
+})
+
+router.post('/logout', (req, res) => {
+    res.cookie('jwt', '', { maxAge: 0 })
+
+    res.send({
+        message: 'you logout'
+    })
 })
 
 
